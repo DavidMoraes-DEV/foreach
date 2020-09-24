0fa2e91b927dcb141fe946b6ed9f6123052c377b
David Moraes <investtraderrs@gmail.com>
2020-09-24 16:51:20 -0300

    foreach

---
+++ LacoForEach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LacoForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             O Laço "foreach" é uma sintaxe opicional e simplificada do
+            "for" padrão para percorrer coleções.
+            Obs. É muito utilizado!!!
+            */
+
+            string[] vetor = new string[] { "Maria", "Bob", "Alex" };
+
+            /*Se lê: "Para cada objeto 'obj' contido em um vetor, faça:
+            (Esse 'obj' serve como um tipo de apelido para cada elemento
+            do vetor, podendo ser outro nome qualquer)"*/
+            foreach (string obj in vetor)
+            {
+                Console.WriteLine(obj);
+                /*Para cada elemento do vetor será executado esse comando*/
+            }
+
+        }
+    }
+}
